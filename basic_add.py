a6cf9b5abaae1fb9bfc1cdf4b61f7f9b6763b3ba
B-VTeja <butnavishnuteja@gmail.com>
2020-12-22 16:51:50 +0530

    added mul def

--- basic_add.py
+++ basic_add.py
@@ -4,7 +4,14 @@ def basic_add(num1, num2):
 def basic_sub(num1, num2):
     return num1 - num2
 
-result = basic_add(10, 20)
-subres = basic_sub(10,20)
-print('addtion result:', result)
-print('subtraction: ', subres)
+def basic_mul(num1, num2):
+    return num1 * num2
+
+addRes = basic_add(10, 20)
+subRes = basic_sub(10,20)
+mulRes = basic_mul(10,20)
+
+print('addtion result:', addRes)
+print('subtraction: ', subRes)
+print('multiplication:', mulRes)
+
