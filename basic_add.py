f85a318aa147cd09f568fe5f1aee82c74e2c2e04
B-VTeja <butnavishnuteja@gmail.com>
2020-12-22 16:41:25 +0530

    modified basic_add.py

--- basic_add.py
+++ basic_add.py
@@ -1,6 +1,10 @@
 def basic_add(num1, num2):
     return num1 + num2
 
-result = basic_add(10, 20)
+def basic_sub(num1, num2):
+    return num1 - num2
 
+result = basic_add(10, 20)
+subres = basic_sub(10,20)
 print('addtion result:', result)
+print('subtraction: ', subres)
